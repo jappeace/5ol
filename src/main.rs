999aae68760ce3208c9e86c98e707719b1f3f894
Jappie Klooster <jappieklooster@hotmail.com>
2016-09-03 10:37:41 -0400

    this will not cuase 100% cpu

--- src/main.rs
+++ src/main.rs
@@ -44,155 +44,88 @@ fn main() {
 
     // Instantiate the generated list of widget identifiers.
     let ids = &mut Ids::new(ui.widget_id_generator());
-
     // Poll events from the window.
-    while let Some(event) = window.next() {
 
-        // Convert the piston event to a conrod event.
+    let mut handledInput = false;
+    while let Some(event) = window.next() {
         if let Some(e) = conrod::backend::piston_window::convert_event(event.clone(), &window) {
             ui.handle_event(e);
         }
-
-        event.update(|_| {
-            ;
-        });
         match event.clone() {
-            Idle (_ ) => println!("idleing"),
-            Render(_ ) => println!("rendering"),
-            AfterRender(_ ) => println!("after render??"),
-            Update(_ ) => set_widgets(ui.set_widgets(), ids),
-            Input(I) => println!("inputing")
-        }
+            Idle (_ ) => std::thread::yield_now(),
+            Render(_ ) => window.draw_2d(&event, |c, g| {
+                    if let Some(primitives) = ui.draw_if_changed() {
+                        fn texture_from_image<T>(img: &T) -> &T { img };
+                        conrod::backend::piston_window::draw(c, g, primitives,
+                                                             &mut text_texture_cache,
+                                                             &image_map,
+                                                             texture_from_image);
+                    }
+                }).unwrap(),
+            AfterRender(_ ) => continue,
+            Update(_ ) => if !handledInput {set_widgets(ui.set_widgets(), ids); handledInput = true },
+            Input(I) => handledInput = false,
+        };
     }
 
-}
-use conrod::Ui;
-use conrod::image::Map;
-use conrod::backend::piston_window::GlyphCache;
-fn render(window:&mut PistonWindow, event:Event, ui:Ui, text_textue_cache:GlyphCache, image_map:Map){
-    window.draw_2d(&event, |c, g| {
-        if let Some(primitives) = ui.draw_if_changed() {
-            fn texture_from_image<T>(img: &T) -> &T { img };
-            conrod::backend::piston_window::draw(c, g, primitives,
-                                                 &mut text_texture_cache,
-                                                 &image_map,
-                                                 texture_from_image);
-        }
-    });
 }
 
 // Draw the Ui.
 fn set_widgets(ref mut ui: conrod::UiCell, ids: &mut Ids) {
-    use conrod::{color, widget, Colorable, Labelable, Positionable, Sizeable, Widget};
+  use conrod::{color, widget, Colorable, Positionable, Scalar, Sizeable, Widget};
 
-    // Construct our main `Canvas` tree.
-    widget::Canvas::new().flow_down(&[
-        (ids.header, widget::Canvas::new().color(color::BLUE).pad_bottom(20.0)),
-        (ids.body, widget::Canvas::new().length(300.0).flow_right(&[
-            (ids.left_column, widget::Canvas::new().color(color::LIGHT_ORANGE).pad(20.0)),
-            (ids.middle_column, widget::Canvas::new().color(color::ORANGE)),
-            (ids.right_column, widget::Canvas::new().color(color::DARK_ORANGE).pad(20.0)),
-        ])),
-        (ids.footer, widget::Canvas::new().color(color::BLUE).scroll_kids_vertically()),
+    // Our `Canvas` tree, upon which we will place our text widgets.
+    widget::Canvas::new().flow_right(&[
+        (ids.left_col, widget::Canvas::new().color(color::BLACK)),
+        (ids.middle_col, widget::Canvas::new().color(color::DARK_CHARCOAL)),
+        (ids.right_col, widget::Canvas::new().color(color::CHARCOAL)),
     ]).set(ids.master, ui);
 
-    // A scrollbar for the `FOOTER` canvas.
-    widget::Scrollbar::y_axis(ids.footer).auto_hide(true).set(ids.footer_scrollbar, ui);
+    const DEMO_TEXT: &'static str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+        Mauris aliquet porttitor tellus vel euismod. Integer lobortis volutpat bibendum. Nulla \
+        finibus odio nec elit condimentum, rhoncus fermentum purus lacinia. Interdum et malesuada \
+        fames ac ante ipsum primis in faucibus. Cras rhoncus nisi nec dolor bibendum pellentesque. \
+        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. \
+        Quisque commodo nibh hendrerit nunc sollicitudin sodales. Cras vitae tempus ipsum. Nam \
+        magna est, efficitur suscipit dolor eu, consectetur consectetur urna.";
 
-    // Now we'll make a couple floating `Canvas`ses.
-    let floating = widget::Canvas::new().floating(true).w_h(110.0, 150.0).label_color(color::WHITE);
-    floating.middle_of(ids.left_column).title_bar("Blue").color(color::BLUE).set(ids.floating_a, ui);
-    floating.middle_of(ids.right_column).title_bar("Orange").color(color::LIGHT_ORANGE).set(ids.floating_b, ui);
+    const PAD: Scalar = 20.0;
 
-    // Here we make some canvas `Tabs` in the middle column.
-    widget::Tabs::new(&[(ids.tab_foo, "FOO"), (ids.tab_bar, "BAR"), (ids.tab_baz, "BAZ")])
-        .wh_of(ids.middle_column)
-        .color(color::BLUE)
-        .label_color(color::WHITE)
-        .middle_of(ids.middle_column)
-        .set(ids.tabs, ui);
+    widget::Text::new(DEMO_TEXT)
+        .color(color::LIGHT_RED)
+        .padded_w_of(ids.left_col, PAD)
+        .mid_top_with_margin_on(ids.left_col, PAD)
+        .align_text_left()
+        .line_spacing(10.0)
+        .set(ids.left_text, ui);
 
-    widget::Text::new("Fancy Title")
-        .color(color::LIGHT_ORANGE)
-        .font_size(48)
-        .middle_of(ids.header)
-        .set(ids.title, ui);
-    widget::Text::new("Subtitle")
-        .color(color::BLUE.complement())
-        .mid_bottom_of(ids.header)
-        .set(ids.subtitle, ui);
+    widget::Text::new(DEMO_TEXT)
+        .color(color::LIGHT_GREEN)
+        .padded_w_of(ids.middle_col, PAD)
+        .middle_of(ids.middle_col)
+        .align_text_middle()
+        .line_spacing(2.5)
+        .set(ids.middle_text, ui);
 
-    widget::Text::new("Top Left")
-        .color(color::LIGHT_ORANGE.complement())
-        .top_left_of(ids.left_column)
-        .set(ids.top_left, ui);
-
-    widget::Text::new("Bottom Right")
-        .color(color::DARK_ORANGE.complement())
-        .bottom_right_of(ids.right_column)
-        .set(ids.bottom_right, ui);
-
-    fn text (text: widget::Text) -> widget::Text { text.color(color::WHITE).font_size(36) }
-    text(widget::Text::new("Foo!")).middle_of(ids.tab_foo).set(ids.foo_label, ui);
-    text(widget::Text::new("Bar!")).middle_of(ids.tab_bar).set(ids.bar_label, ui);
-    text(widget::Text::new("BAZ!")).middle_of(ids.tab_baz).set(ids.baz_label, ui);
-
-    let footer_wh = ui.wh_of(ids.footer).unwrap();
-    let mut elements = widget::Matrix::new(COLS, ROWS)
-        .w_h(footer_wh[0], footer_wh[1] * 2.0)
-        .mid_top_of(ids.footer)
-        .set(ids.button_matrix, ui);
-    while let Some(elem) = elements.next(ui) {
-        let (r, c) = (elem.row, elem.col);
-        let n = c + r * c;
-        let luminance = n as f32 / (COLS * ROWS) as f32;
-        let button = widget::Button::new().color(color::BLUE.with_luminance(luminance));
-        for _click in elem.set(button, ui) {
-            println!("Hey! {:?}", (r, c));
-        }
-    }
-
-    let button = widget::Button::new().color(color::RED).w_h(30.0, 30.0);
-    for _click in button.clone().middle_of(ids.floating_a).set(ids.bing, ui) {
-        println!("Bing!");
-    }
-    for _click in button.middle_of(ids.floating_b).set(ids.bong, ui) {
-        println!("Bong!");
-    }
+    widget::Text::new(DEMO_TEXT)
+        .color(color::LIGHT_BLUE)
+        .padded_w_of(ids.right_col, PAD)
+        .mid_bottom_with_margin_on(ids.right_col, PAD)
+        .align_text_right()
+        .line_spacing(5.0)
+.set(ids.right_text, ui);
 }
 
 
-// Button matrix dimensions.
-const ROWS: usize = 10;
-const COLS: usize = 24;
-
-// Generate a unique `WidgetId` for each widget.
-widget_ids! {
+// Generate a unique const `WidgetId` for each widget.
+widget_ids!{
     Ids {
         master,
-        header,
-        body,
-        left_column,
-        middle_column,
-        right_column,
-        footer,
-        footer_scrollbar,
-        floating_a,
-        floating_b,
-        tabs,
-        tab_foo,
-        tab_bar,
-        tab_baz,
-
-        title,
-        subtitle,
-        top_left,
-        bottom_right,
-        foo_label,
-        bar_label,
-        baz_label,
-        button_matrix,
-        bing,
-        bong,
+        left_col,
+        middle_col,
+        right_col,
+        left_text,
+        middle_text,
+        right_text,
     }
 }
