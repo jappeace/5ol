1c6e115709e6543c92a1e816d7648e2406adb512
Jappie Klooster <jappieklooster@hotmail.com>
2016-09-03 13:13:11 -0400

    Intro text and proper window name

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use piston_window::Event::*;
 const assetspath: &'static str = "assets";
 const font: &'static str = "fonts/NotoSans/NotoSans-Regular.ttf";
 
+const NAME: &'static str = env!("CARGO_PKG_NAME");
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 fn main() {
     const WIDTH: u32 = 800;
@@ -19,7 +21,7 @@ fn main() {
     
     // Construct the window.
     let mut window: PistonWindow =
-        WindowSettings::new("Canvas Demo", [WIDTH, HEIGHT])
+        WindowSettings::new(format!("{} - {}", NAME, VERSION), [WIDTH, HEIGHT])
             .opengl(opengl).exit_on_esc(true).vsync(true).build().unwrap();
     window.set_ups(60);
     window.set_max_fps(60);
@@ -72,84 +74,25 @@ fn main() {
 
 // Draw the Ui.
 fn set_widgets(ref mut ui: conrod::UiCell, ids: &mut Ids) {
-    use conrod::{color, widget, Colorable, Labelable, Positionable, Sizeable, Widget};
+    use conrod::{color, widget, Colorable, Labelable, Positionable, Sizeable, Widget, Scalar};
     use conrod::widget::Line;
 
+
+    const PAD: Scalar = 20.0;
+    const DEMO_TEXT: &'static str = "After a long power struggle over now several generations, 
+    you Kim Ill Sung III has finally crushed all opposition.
+    With no-one else left daring to oppose you mankind puts it faith in you,
+    our dearest leader, and you look to the stars...
+";
     // Construct our main `Canvas` tree.
-    widget::Canvas::new().flow_down(&[
-        (ids.header, widget::Canvas::new().color(color::BLUE).pad_bottom(90.0)),
-        (ids.body, widget::Canvas::new().length(300.0).flow_right(&[
-            (ids.left_column, widget::Canvas::new().color(color::LIGHT_ORANGE).pad(20.0)),
-            (ids.middle_column, widget::Canvas::new().color(color::ORANGE)),
-            (ids.right_column, widget::Canvas::new().color(color::DARK_ORANGE).pad(50.0)),
-        ])),
-        (ids.footer, widget::Canvas::new().color(color::BLUE).scroll_kids_vertically()),
-    ]).set(ids.master, ui);
-
-    Line::centred([-40.0, -40.0], [40.0, 40.0]).top_left_of(ids.header).set(ids.line, ui);
-    // A scrollbar for the `FOOTER` canvas.
-    widget::Scrollbar::y_axis(ids.footer).auto_hide(true).set(ids.footer_scrollbar, ui);
-
-    // Now we'll make a couple floating `Canvas`ses.
-    let floating = widget::Canvas::new().floating(true).w_h(110.0, 150.0).label_color(color::WHITE);
-    floating.middle_of(ids.left_column).title_bar("Blue").color(color::BLUE).set(ids.floating_a, ui);
-    floating.middle_of(ids.right_column).title_bar("Orange").color(color::LIGHT_ORANGE).set(ids.floating_b, ui);
-
-    // Here we make some canvas `Tabs` in the middle column.
-    widget::Tabs::new(&[(ids.tab_foo, "FOO"), (ids.tab_bar, "BAR"), (ids.tab_baz, "BAZ")])
-        .wh_of(ids.middle_column)
-        .color(color::BLUE)
-        .label_color(color::WHITE)
-        .middle_of(ids.middle_column)
-        .set(ids.tabs, ui);
-
-    widget::Text::new("Fancy Title")
-        .color(color::LIGHT_ORANGE)
-        .font_size(48)
-        .middle_of(ids.header)
-        .set(ids.title, ui);
-    widget::Text::new("Subtitle")
-        .color(color::BLUE.complement())
-        .mid_bottom_of(ids.header)
-        .set(ids.subtitle, ui);
-
-    widget::Text::new("Top Left")
-        .color(color::LIGHT_ORANGE.complement())
-        .top_left_of(ids.left_column)
-        .set(ids.top_left, ui);
-
-    widget::Text::new("Bottom Right")
-        .color(color::DARK_ORANGE.complement())
-        .bottom_right_of(ids.right_column)
-        .set(ids.bottom_right, ui);
-
-    fn text (text: widget::Text) -> widget::Text { text.color(color::WHITE).font_size(36) }
-    text(widget::Text::new("Foo!")).middle_of(ids.tab_foo).set(ids.foo_label, ui);
-    text(widget::Text::new("Bar!")).middle_of(ids.tab_bar).set(ids.bar_label, ui);
-    text(widget::Text::new("BAZ!")).middle_of(ids.tab_baz).set(ids.baz_label, ui);
-
-    let footer_wh = ui.wh_of(ids.footer).unwrap();
-    let mut elements = widget::Matrix::new(COLS, ROWS)
-        .w_h(footer_wh[0], footer_wh[1] * 2.0)
-        .mid_top_of(ids.footer)
-        .set(ids.button_matrix, ui);
-    while let Some(elem) = elements.next(ui) {
-        let (r, c) = (elem.row, elem.col);
-        let n = c + r * c;
-        let luminance = n as f32 / (COLS * ROWS) as f32;
-        let button = widget::Button::new().color(color::BLUE.with_luminance(luminance));
-        for _click in elem.set(button, ui) {
-            println!("Hey! {:?}", (r, c));
-        }
-    }
-
-    let button = widget::Button::new().color(color::RED).w_h(30.0, 30.0);
-    for _click in button.clone().middle_of(ids.floating_a).set(ids.bing, ui) {
-        println!("Bing!");
-    }
-    for _click in button.middle_of(ids.floating_b).set(ids.bong, ui) {
-        println!("Bong!");
-    }
+    widget::Canvas::new().color(color::BLACK).set(ids.root, ui);
+    widget::Text::new(DEMO_TEXT)
+        .color(color::LIGHT_RED)
+        .padded_w_of(ids.root, PAD)
+        .mid_top_with_margin_on(ids.root, PAD)
+        .align_text_left()
+        .line_spacing(10.0)
+        .set(ids.intro_text, ui);
 }
 
 
@@ -160,31 +103,7 @@ const COLS: usize = 24;
 // Generate a unique `WidgetId` for each widget.
 widget_ids! {
     Ids {
-        master,
-        header,
-        body,
-        left_column,
-        middle_column,
-        right_column,
-        footer,
-        footer_scrollbar,
-        floating_a,
-        floating_b,
-        tabs,
-        tab_foo,
-        tab_bar,
-        tab_baz,
-
-        title,
-        subtitle,
-        top_left,
-        bottom_right,
-        foo_label,
-        bar_label,
-        baz_label,
-        button_matrix,
-        bing,
-        bong,
-        line
+        root,
+        intro_text
     }
 }
