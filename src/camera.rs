88ce9d71bb0c008477010f71921fe1d98a8fa08a
Jappie Klooster <jappieklooster@hotmail.com>
2018-09-05 09:14:17 +1000

    Rustfmt

--- src/camera.rs
+++ src/camera.rs
@@ -14,138 +14,140 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.If not, see <http://www.gnu.org/licenses/>.
 
-
 // does the world to screen mapping, ie determine where a world coordinate
 // (au,au), should be rendered in (px,px)
 
+use conrod::Dimensions;
 use geometry::*;
 use model::galaxy::{Au, BodyAddress};
-use conrod::Dimensions;
 
-pub enum ZoomDirection{
+pub enum ZoomDirection {
     In,
-    Out
+    Out,
 }
-pub enum MoveDirection{
+pub enum MoveDirection {
     Left,
     Right,
     Up,
-    Down
+    Down,
 }
-const zoom_factor:f64 = 2.0;
-const move_step:f64 = 0.05;
-pub const start_cam_width:Au = 2.0;
-pub const start_cam_height:Au = 2.0;
-pub struct Camera{
-    pub position:Position, // position in world coordinates (AU)
-    pub width:Au, // in astromical units
-    pub height:Au,
-    pub track_body:Option<BodyAddress>
+const zoom_factor: f64 = 2.0;
+const move_step: f64 = 0.05;
+pub const start_cam_width: Au = 2.0;
+pub const start_cam_height: Au = 2.0;
+pub struct Camera {
+    pub position: Position, // position in world coordinates (AU)
+    pub width: Au,          // in astromical units
+    pub height: Au,
+    pub track_body: Option<BodyAddress>,
 }
-impl Camera{
-    pub fn new(position:Position, width:Au, height:Au)->Camera{
-        Camera{
-            position:position,
-            width:width,
-            height:height,
-            track_body:None
+impl Camera {
+    pub fn new(position: Position, width: Au, height: Au) -> Camera {
+        Camera {
+            position: position,
+            width: width,
+            height: height,
+            track_body: None,
         }
     }
-    pub fn stop_tracking(&mut self){
+    pub fn stop_tracking(&mut self) {
         self.track_body = None;
     }
-    pub fn translate(&mut self, direction:MoveDirection){
+    pub fn translate(&mut self, direction: MoveDirection) {
         let zero = 0.0;
-        let movement = match direction{
-            MoveDirection::Left =>  Position::new(move_step, zero),
+        let movement = match direction {
+            MoveDirection::Left => Position::new(move_step, zero),
             MoveDirection::Right => Position::new(-move_step, zero),
             MoveDirection::Up => Position::new(zero, -move_step),
-            MoveDirection::Down => Position::new(zero, move_step)
+            MoveDirection::Down => Position::new(zero, move_step),
         };
         let scaled_movement = movement * Position::new(self.width, self.height);
         self.position += scaled_movement;
         self.stop_tracking();
     }
-    pub fn zoom(&mut self, screen_size:Dimensions, direction:ZoomDirection, mouse_position:Position){
-
+    pub fn zoom(
+        &mut self,
+        screen_size: Dimensions,
+        direction: ZoomDirection,
+        mouse_position: Position,
+    ) {
         let mouse = mouse_position - (Position::arr(screen_size) / Position::i(2));
-        let desired_fixed_point = self.create_projection(screen_size)
-            .screen_to_world(mouse);
+        let desired_fixed_point = self.create_projection(screen_size).screen_to_world(mouse);
 
-        match direction{
-            ZoomDirection::In =>{
+        match direction {
+            ZoomDirection::In => {
                 self.width /= zoom_factor;
                 self.height /= zoom_factor;
             }
-            ZoomDirection::Out =>{
+            ZoomDirection::Out => {
                 self.width *= zoom_factor;
                 self.height *= zoom_factor;
             }
-
         }
 
-        let fixed_move = self.create_projection(screen_size)
-            .screen_to_world(mouse);
+        let fixed_move = self.create_projection(screen_size).screen_to_world(mouse);
         let correction_movement = fixed_move - desired_fixed_point;
 
-        println!("move {}, mouse {}", correction_movement, desired_fixed_point);
-        self.position += correction_movement * Position::new(-1.0,1.0);
+        println!(
+            "move {}, mouse {}",
+            correction_movement, desired_fixed_point
+        );
+        self.position += correction_movement * Position::new(-1.0, 1.0);
     }
-    pub fn create_projection(&self, screen_size: Dimensions) -> Projection{
+    pub fn create_projection(&self, screen_size: Dimensions) -> Projection {
         let two = 2.0;
-        Projection{
-            view_port:Rectangle{
-                one: Position{
-                    x:self.position.x-self.width/two,
-                    y:self.position.y-self.height/two,
+        Projection {
+            view_port: Rectangle {
+                one: Position {
+                    x: self.position.x - self.width / two,
+                    y: self.position.y - self.height / two,
                 },
-                two: Position{
-                    x:self.position.x+self.width/two,
-                    y:self.position.y+self.height/two,
-                }
+                two: Position {
+                    x: self.position.x + self.width / two,
+                    y: self.position.y + self.height / two,
+                },
             },
-            screen_size:screen_size
+            screen_size: screen_size,
         }
     }
 }
-pub struct Projection{
-    pub view_port:Rectangle,
-    screen_size:Dimensions
-
+pub struct Projection {
+    pub view_port: Rectangle,
+    screen_size: Dimensions,
 }
-impl Projection{
-    pub fn get_screen_viewport_ratio(&self) -> Position{
-        Position::new(self.screen_size[0], self.screen_size[1]) /
-            Position::new(self.view_port.width(), self.view_port.height())
+impl Projection {
+    pub fn get_screen_viewport_ratio(&self) -> Position {
+        Position::new(self.screen_size[0], self.screen_size[1])
+            / Position::new(self.view_port.width(), self.view_port.height())
     }
-    pub fn screen_to_world(&self, position:Position)->Position{
+    pub fn screen_to_world(&self, position: Position) -> Position {
         let ratio = self.get_screen_viewport_ratio();
         (position) / ratio - self.view_port.center()
     }
-    pub fn world_to_screen(&self, position:Position)->Position{
+    pub fn world_to_screen(&self, position: Position) -> Position {
         (position - self.view_port.center()) * self.get_screen_viewport_ratio()
     }
-    pub fn is_visible(&self, disk:&Disk) -> bool{
+    pub fn is_visible(&self, disk: &Disk) -> bool {
         let (tl, tr, bl, br) = self.view_port.corners();
-        self.is_pos_visible(&disk.position) ||
-            disk.contains([tl, tr]) ||
-            disk.contains([tr, br]) ||
-            disk.contains([br, bl]) ||
-            disk.contains([bl, tl])
+        self.is_pos_visible(&disk.position)
+            || disk.contains([tl, tr])
+            || disk.contains([tr, br])
+            || disk.contains([br, bl])
+            || disk.contains([bl, tl])
     }
-    pub fn is_pos_visible(&self, pos:&Position) -> bool{
+    pub fn is_pos_visible(&self, pos: &Position) -> bool {
         let result = self.view_port.contains(pos);
         result
     }
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use camera::*;
     use geometry::*;
     #[test]
-    fn projection_idompotency(){
-        let mut cam = Camera::new(Position{x:3.0, y:59.3}, 4.0, 2.1);
+    fn projection_idompotency() {
+        let mut cam = Camera::new(Position { x: 3.0, y: 59.3 }, 4.0, 2.1);
         let some_screensize = [100.0, 250.3]; // floating point may make epsilon differences
         let projection = cam.create_projection(some_screensize);
         let some_point = Position::new(49.0, -239.5);
@@ -159,43 +161,45 @@ mod tests{
         )
     }
     #[test]
-    fn ships_should_be_visible_in_start_location(){
+    fn ships_should_be_visible_in_start_location() {
+        use chrono::Duration;
         use model::galaxy::*;
         use model::root::*;
         use model::ship::*;
-        use chrono::Duration;
         // there is an annoying filter bug where ships are only sometimes
         // visible on the lower zoom levels
         // its really hard to test ingame so this unit test does it
         // programaticially
         let gamemodel = {
             let ship = {
-                let mut s = Ship::new(0,0,BodyAddress{system_id:0,planet_id:0});
+                let mut s = Ship::new(
+                    0,
+                    0,
+                    BodyAddress {
+                        system_id: 0,
+                        planet_id: 0,
+                    },
+                );
                 s.id = 0;
                 s
             };
-            let mut model = GameModel::new(vec![
-                System::new(center, vec![StellarBody::new_earthlike("earth")])
-            ]);   
+            let mut model = GameModel::new(vec![System::new(
+                center,
+                vec![StellarBody::new_earthlike("earth")],
+            )]);
             model.ships = vec![ship];
             model
         };
-        let mut camera = Camera::new(
-            center,
-            start_cam_width,
-            start_cam_height
-        );
+        let mut camera = Camera::new(center, start_cam_width, start_cam_height);
         let projection = camera.create_projection([300.0, 600.0]);
 
-        let some_visible_days = [45,135,215,305, 345];
-        for day in some_visible_days.iter(){
-            let position = gamemodel.ships[0].movement.calc_position(
-                &Duration::days(*day), &gamemodel.galaxy
-            );
+        let some_visible_days = [45, 135, 215, 305, 345];
+        for day in some_visible_days.iter() {
+            let position = gamemodel.ships[0]
+                .movement
+                .calc_position(&Duration::days(*day), &gamemodel.galaxy);
             println!("position {} for day {}", position, day);
-            assert!(
-                projection.is_pos_visible(&position)
-            )
+            assert!(projection.is_pos_visible(&position))
         }
     }
 }
--- src/geometry.rs
+++ src/geometry.rs
@@ -14,204 +14,237 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.If not, see <http://www.gnu.org/licenses/>.
 
-
 // this file describes some geometric constructs, the main unit being position.
 // I wrote my own to use f64 instead of f32, simply because the entire milky
 // way sortoff fits in that if we use AU as a unit
-#[derive(Copy,Clone, PartialEq, Debug)]
-pub struct Position{
+#[derive(Copy, Clone, PartialEq, Debug)]
+pub struct Position {
     // left = 0
-    pub x:f64,
+    pub x: f64,
     // top = 0
-    pub y:f64
+    pub y: f64,
 }
-use std::ops::{Add, AddAssign, Sub, SubAssign, Neg, Div, Mul};
 use conrod::Dimensions;
-impl Position{
-    pub fn new(x:f64,y:f64) -> Position{
-        Position{
-            x:x,
-            y:y
-        }
+use std::ops::{Add, AddAssign, Div, Mul, Neg, Sub, SubAssign};
+impl Position {
+    pub fn new(x: f64, y: f64) -> Position {
+        Position { x: x, y: y }
     }
-    pub fn arr(dimension:Dimensions) -> Position{
+    pub fn arr(dimension: Dimensions) -> Position {
         Position::new(dimension[0], dimension[1])
     }
-    pub fn is(x:isize, y:isize) -> Position{
-        Position::new(x as f64,y as f64)
+    pub fn is(x: isize, y: isize) -> Position {
+        Position::new(x as f64, y as f64)
     }
-    pub fn i(size:isize) -> Position{
-        Position::is(size,size)
+    pub fn i(size: isize) -> Position {
+        Position::is(size, size)
     }
-    fn dot(&self, with:Position) -> f64{
+    fn dot(&self, with: Position) -> f64 {
         self.length() * with.length() * self.angle_rad(with).cos()
     }
-    fn length_sq(&self)->f64{
-        self.x*self.x+self.y*self.y
+    fn length_sq(&self) -> f64 {
+        self.x * self.x + self.y * self.y
     }
-    fn length(&self)->f64{
+    fn length(&self) -> f64 {
         self.length_sq().sqrt()
     }
-    fn angle_rad(&self, other:Position) -> f64{
+    fn angle_rad(&self, other: Position) -> f64 {
         (self.y - other.y).atan2(self.x - other.x)
     }
 }
 use std::fmt;
-impl fmt::Display for Position{
+impl fmt::Display for Position {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "({}, {})", self.x, self.y)
     }
 }
-impl Add for Position{
+impl Add for Position {
     type Output = Position;
-    fn add(self, _rhs: Position) -> Position{   
-        Position{x:_rhs.x+self.x, y:_rhs.y+self.y}
+    fn add(self, _rhs: Position) -> Position {
+        Position {
+            x: _rhs.x + self.x,
+            y: _rhs.y + self.y,
+        }
     }
 }
-impl AddAssign for Position{
-    fn add_assign(&mut self, _rhs:Position){
+impl AddAssign for Position {
+    fn add_assign(&mut self, _rhs: Position) {
         self.x += _rhs.x;
         self.y += _rhs.y;
     }
 }
-impl Mul for Position{
+impl Mul for Position {
     type Output = Position;
-    fn mul(self, _rhs:Position) -> Position{
-        Position::new(self.x*_rhs.x,self.y*_rhs.y)
+    fn mul(self, _rhs: Position) -> Position {
+        Position::new(self.x * _rhs.x, self.y * _rhs.y)
     }
 }
-impl Div for Position{
+impl Div for Position {
     type Output = Position;
-    fn div(self, _rhs:Position) -> Position{
-        Position::new(self.x/_rhs.x,self.y/_rhs.y)
+    fn div(self, _rhs: Position) -> Position {
+        Position::new(self.x / _rhs.x, self.y / _rhs.y)
     }
 }
-impl Sub for Position{
+impl Sub for Position {
     type Output = Position;
-    fn sub(self, _rhs: Position) -> Position{   
-        Position{x:_rhs.x-self.x, y:_rhs.y-self.y}
+    fn sub(self, _rhs: Position) -> Position {
+        Position {
+            x: _rhs.x - self.x,
+            y: _rhs.y - self.y,
+        }
     }
 }
-impl SubAssign for Position{
-    fn sub_assign(&mut self, _rhs:Position){
+impl SubAssign for Position {
+    fn sub_assign(&mut self, _rhs: Position) {
         self.x -= _rhs.x;
         self.y -= _rhs.y;
     }
 }
-impl Neg for Position{
+impl Neg for Position {
     type Output = Position;
-    fn neg(self) -> Position{   
-        Position{x:-self.x, y:-self.y}
+    fn neg(self) -> Position {
+        Position {
+            x: -self.x,
+            y: -self.y,
+        }
     }
 }
 
-pub type Line = [Position;2];
+pub type Line = [Position; 2];
 
 #[derive(Clone)]
-pub struct Disk{
-    pub position:Position,
-    pub radius:f64
+pub struct Disk {
+    pub position: Position,
+    pub radius: f64,
 }
-impl Disk{
-    pub fn contains(&self, line:Line) -> bool{
+impl Disk {
+    pub fn contains(&self, line: Line) -> bool {
         let d = line[0] - line[1];
         let f = line[1] - self.position;
         let r = self.radius;
-        let a = d.dot( d ) ;
-        let b = 2.0*f.dot( d ) ;
-        let c = f.dot( f ) - r*r ;
+        let a = d.dot(d);
+        let b = 2.0 * f.dot(d);
+        let c = f.dot(f) - r * r;
 
-        let discriminant = b*b-4.0*a*c;
+        let discriminant = b * b - 4.0 * a * c;
         discriminant >= 0.0
     }
 }
 
 // give me two points and I give you a rectangle
 pub struct Rectangle {
-    pub one:Position,
-    pub two:Position
+    pub one: Position,
+    pub two: Position,
 }
-impl Rectangle{
-    pub fn contains(&self, position:&Position) -> bool{
+impl Rectangle {
+    pub fn contains(&self, position: &Position) -> bool {
         let tl = self.tl();
         let br = self.br();
         tl.x < position.x && tl.y < position.y && br.x > position.x && br.y > position.y
     }
-    pub fn width(&self) -> f64{
+    pub fn width(&self) -> f64 {
         (self.one.x - self.two.x).abs()
     }
-    pub fn height(&self) -> f64{
+    pub fn height(&self) -> f64 {
         (self.one.y - self.two.y).abs()
     }
-    pub fn center(&self) -> Position{
+    pub fn center(&self) -> Position {
         let tl = self.tl();
         let two = 2.0;
-        Position{
-            x:(self.width()/two) + tl.x,
-            y: (self.height()/two) + tl.y
+        Position {
+            x: (self.width() / two) + tl.x,
+            y: (self.height() / two) + tl.y,
         }
     }
-    pub fn tl(&self) -> Position{
-        Position{
-            x: if self.one.x < self.two.x {self.one.x} else {self.two.x},
-            y: if self.one.y < self.two.y {self.one.y} else {self.two.y}
+    pub fn tl(&self) -> Position {
+        Position {
+            x: if self.one.x < self.two.x {
+                self.one.x
+            } else {
+                self.two.x
+            },
+            y: if self.one.y < self.two.y {
+                self.one.y
+            } else {
+                self.two.y
+            },
         }
     }
-    pub fn tr(&self) -> Position{
-        Position{
-            x: if self.one.x > self.two.x {self.one.x} else {self.two.x},
-            y: if self.one.y < self.two.y {self.one.y} else {self.two.y}
+    pub fn tr(&self) -> Position {
+        Position {
+            x: if self.one.x > self.two.x {
+                self.one.x
+            } else {
+                self.two.x
+            },
+            y: if self.one.y < self.two.y {
+                self.one.y
+            } else {
+                self.two.y
+            },
         }
     }
-    pub fn br(&self) -> Position{
-        Position{
-            x: if self.one.x > self.two.x {self.one.x} else {self.two.x},
-            y: if self.one.y > self.two.y {self.one.y} else {self.two.y}
+    pub fn br(&self) -> Position {
+        Position {
+            x: if self.one.x > self.two.x {
+                self.one.x
+            } else {
+                self.two.x
+            },
+            y: if self.one.y > self.two.y {
+                self.one.y
+            } else {
+                self.two.y
+            },
         }
     }
-    pub fn bl(&self) -> Position{
-        Position{
-            x: if self.one.x < self.two.x {self.one.x} else {self.two.x},
-            y: if self.one.y > self.two.y {self.one.y} else {self.two.y}
+    pub fn bl(&self) -> Position {
+        Position {
+            x: if self.one.x < self.two.x {
+                self.one.x
+            } else {
+                self.two.x
+            },
+            y: if self.one.y > self.two.y {
+                self.one.y
+            } else {
+                self.two.y
+            },
         }
     }
-    pub fn corners(&self) -> (Position, Position, Position, Position){
+    pub fn corners(&self) -> (Position, Position, Position, Position) {
         (self.tl(), self.tr(), self.bl(), self.br())
     }
 }
-impl fmt::Display for Rectangle{
+impl fmt::Display for Rectangle {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "R({}, {})", self.tl(), self.br())
     }
 }
-pub const center:Position = Position{x:0.0,y:0.0};
+pub const center: Position = Position { x: 0.0, y: 0.0 };
 
 #[cfg(test)]
-mod tests{
-    use geometry::{Position,Rectangle};
+mod tests {
+    use geometry::{Position, Rectangle};
     #[test]
-    fn rectangle_contains(){
+    fn rectangle_contains() {
         let point = Position::i(6);
-        let rectangle = Rectangle{
-            one:Position::i(5),
-            two:Position::i(7),
+        let rectangle = Rectangle {
+            one: Position::i(5),
+            two: Position::i(7),
         };
         println!("tl {}", rectangle.tl());
         println!("br {}", rectangle.br());
         assert!(rectangle.contains(&point));
     }
     #[test]
-    fn rectangle_corners(){
+    fn rectangle_corners() {
         let tl = Position::i(5);
         let br = Position::i(7);
-        let rectangle = Rectangle{
-            one:tl,
-            two:br,
-        };
+        let rectangle = Rectangle { one: tl, two: br };
         assert_eq!(tl, rectangle.tl());
         assert_eq!(br, rectangle.br());
-        assert_eq!(Position::is(7,5), rectangle.tr());
-        assert_eq!(Position::is(5,7), rectangle.bl());
+        assert_eq!(Position::is(7, 5), rectangle.tr());
+        assert_eq!(Position::is(5, 7), rectangle.bl());
     }
 }
- 
